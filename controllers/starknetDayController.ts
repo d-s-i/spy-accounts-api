cdf6ca50eebe3d9d2a250f7754880eb2b1a28218
d-s-i <briacgoujon95@gmail.com>
2022-05-18 18:07:43 +0200

    add few comments

--- controllers/starknetDayController.ts
+++ controllers/starknetDayController.ts
@@ -8,6 +8,66 @@ import {
 
 import { ApiActions } from "../utils/api/ApiActions";
 
+///////////////////////////
+// getters
+export const getStarknetDay = async function(req: express.Request, res: express.Response) {
+    const apiActions = new ApiActions(res);
+    try {
+        const starknetDay = await apiActions.findStarknetDay(req.params.date);
+
+        apiActions.sendGetSuccessful({
+            starknetDay
+        });
+
+    } catch(error) {
+        apiActions.sendError(error);
+    }
+}
+
+export const getBlocksPerDate = async function(req: express.Request, res: express.Response) {
+    const apiActions = new ApiActions(res);
+    try {
+        const starknetDay = await apiActions.findStarknetDay(req.params.date);
+
+        apiActions.sendGetSuccessful({
+            blocks: starknetDay.blocks
+        });
+
+    } catch(error) {
+        apiActions.sendError(error);
+    }
+}
+
+export const getSortedContractsActivityPerDate = async function(req: express.Request, res: express.Response) {
+    const apiActions = new ApiActions(res);
+    try {
+        const starknetDay = await apiActions.findStarknetDay(req.params.date);
+
+        apiActions.sendGetSuccessful({
+            sortedContractsActivity: starknetDay.sortedContractsActivity
+        });
+
+    } catch(error) {
+        apiActions.sendError(error);
+    }
+}
+
+export const getOrganizedAccountsActivityPerDate = async function(req: express.Request, res: express.Response) {
+    const apiActions = new ApiActions(res);
+    try {
+        const starknetDay = await apiActions.findStarknetDay(req.params.date);
+
+        apiActions.sendGetSuccessful({ 
+            organizedAccountsActivity: starknetDay.organizedAccountsActivity
+        });
+
+    } catch(error) {
+        apiActions.sendError(error);
+    }
+}
+
+///////////////////////////
+// posters
 export const addYesterdayStarknetDay = async function(req: express.Request, res: express.Response) {
     const apiActions = new ApiActions(res);
     
@@ -33,69 +93,14 @@ export const addYesterdayStarknetDay = async function(req: express.Request, res:
     }
 }
 
-export const getStarknetDay = async function(req: express.Request, res: express.Response) {
-    const apiActions = new ApiActions(res);
-    try {
-        const starknetDay = await apiActions.findStarknetDay(req.params.date);
-
-        apiActions.sendGetSuccessful({
-            starknetDay
-        });
-
-    } catch(error) {
-        apiActions.sendError(error);
-    }
-}
-
-export const deleteStarknetDay = async function(req: express.Request, res: express.Response) {
-    const apiActions = new ApiActions(res);
-    try {
-       await apiActions.deleteStarknetDay(req.params.date);
-
-        apiActions.sendDeleteSuccessfull();
-
-    } catch(error) {
-        apiActions.sendError(error);
-    }
-    
-}
-
-export const getBlocksPerDate = async function(req: express.Request, res: express.Response) {
-    const apiActions = new ApiActions(res);
-    try {
-        const starknetDay = await apiActions.findStarknetDay(req.params.date);
-
-        apiActions.sendGetSuccessful({
-            blocks: starknetDay.blocks
-        });
-
-    } catch(error) {
-        apiActions.sendError(error);
-    }
-}
-
 export const addBlocksPerDate = async function(req: express.Request, res: express.Response) {
-        const apiActions = new ApiActions(res);
-        const starknetDay = await findStarknetDay(req.params.date);
-
-        if(!starknetDay) {
-            await apiActions.createStarknetDayWithBlocks();
-        } else {
-            apiActions.sendAlreadyQueriedResponse();
-        }
-}
-
-export const getSortedContractsActivityPerDate = async function(req: express.Request, res: express.Response) {
     const apiActions = new ApiActions(res);
-    try {
-        const starknetDay = await apiActions.findStarknetDay(req.params.date);
+    const starknetDay = await findStarknetDay(req.params.date);
 
-        apiActions.sendGetSuccessful({
-            sortedContractsActivity: starknetDay.sortedContractsActivity
-        });
-
-    } catch(error) {
-        apiActions.sendError(error);
+    if(!starknetDay) {
+        await apiActions.createStarknetDayWithBlocks();
+    } else {
+        apiActions.sendAlreadyQueriedResponse();
     }
 }
 
@@ -118,16 +123,17 @@ export const addSortedContractsActivityPerDate = async function(req: express.Req
     }
 }
 
-export const getOrganizedAccountsActivityPerDate = async function(req: express.Request, res: express.Response) {
+///////////////////////////
+// deleters
+export const deleteStarknetDay = async function(req: express.Request, res: express.Response) {
     const apiActions = new ApiActions(res);
     try {
-        const starknetDay = await apiActions.findStarknetDay(req.params.date);
+       await apiActions.deleteStarknetDay(req.params.date);
 
-        apiActions.sendGetSuccessful({ 
-            organizedAccountsActivity: starknetDay.organizedAccountsActivity
-        });
+        apiActions.sendDeleteSuccessfull();
 
     } catch(error) {
         apiActions.sendError(error);
     }
+    
 }
