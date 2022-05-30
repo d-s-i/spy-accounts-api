0dc6ab54968095f03035c9701248995a78aca150
d-s-i <briacgoujon95@gmail.com>
2022-05-30 16:41:32 +0200

    push to test something

--- models/organized/organizedAccountsActivityModel.ts
+++ models/organized/organizedAccountsActivityModel.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { RawTransactionsModel } from "../raw/rawTransactionsModel";
 import { OrganizedTransactionModel } from "./organizedTransactionModel";
 
+// test
 const organizedAccountsActivitySchema = new mongoose.Schema({
     transactionCount: {
         type: Number,
