f5bebf53c3b0bdf53405083747167d555c22ef79
d-s-i <briacgoujon95@gmail.com>
2022-05-31 09:01:07 +0200

    changing CORS url

--- app.ts
+++ app.ts
@@ -4,10 +4,11 @@ import starknetDayRouter from "./routes/starknetDayRouter";
 const app = express();
 app.use(express.json());
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     // res.setHeader("Access-Control-Allow-Origin", "https://vast-lowlands-69461.herokuapp.com");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    // res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.setHeader("Access-Control-Allow-Origin", "https://https://spy-accounts-website.vercel.app/");
+    // res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
     next();
 });
 
