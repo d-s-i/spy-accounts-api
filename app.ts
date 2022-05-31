bf0a8ceaab581487f08f696a06be7de5d5b1a71f
d-s-i <briacgoujon95@gmail.com>
2022-05-31 09:25:15 +0200

    changing CORS

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
     // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     // res.setHeader("Access-Control-Allow-Origin", "https://vast-lowlands-69461.herokuapp.com");
-    res.setHeader("Access-Control-Allow-Origin", "https://spy-accounts-website.vercel.app/");
+    res.setHeader("Access-Control-Allow-Origin", "https://spy-accounts-website.vercel.app");
     // res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Methods", "GET");
     next();
